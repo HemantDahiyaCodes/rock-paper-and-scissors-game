d02121c66f7e1c6e163eb1760771d6a2f6e897a2
HemantDahiyaCodes <HemnatDahiyaCodes@gmail.com>
2024-09-23 21:41:55 +0530

    Created the design of the game

---
+++ main.js
@@ -0,0 +1,7 @@
+// Getting buttons and displays
+const rockIcon = document.getElementById("rock");
+const paperIcon = document.getElementById("paper");
+const scissorsIcon = document.getElementById("scissors");
+const myScore = document.querySelector(".human-score");
+const computerScore = document.querySelector(".computer-score");
+const winnerText = document.querySelector(".winner");
--- script.js
+++ script.js
@@ -40,56 +40,71 @@ const getHumanChoice = () => {
 // console.log(playARound(getHumanChoice(), getComputerChoice()));
 // Play the game for 5 rounds.
 const playGame = () => {
-  let humanScore = 0;
-  let computerScore = 0;
-  const result = [];
-  const playARound = (humanChoice, computerChoice) => {
-    if (humanChoice === computerChoice) {
-      console.log(`It's a tie. You chose ${humanChoice} and the computer chose ${computerChoice}`);
-    }
-    switch (humanChoice) {
-      case "rock":
-        if (computerChoice === "scissors") {
-          humanScore += 1;
-          console.log(`You won, You chose ${humanChoice} and computer chose ${computerChoice}`);
-        } else if (computerChoice === "paper") {
-          computerScore += 1;
-          console.log(`Computer won, You chose ${humanChoice} and computer chose ${computerChoice}`);
-        }
-        break;
-
-      case "paper":
-        if (computerChoice === "rock") {
-          humanScore += 1;
-          console.log(`You won, You chose ${humanChoice} and computer chose ${computerChoice}`);
-        } else if (computerChoice === "scissors") {
-          computerScore += 1;
-          console.log(`Computer won, You chose ${humanChoice} and computer chose ${computerChoice}`);
-        }
-        break;
-      case "scissors":
-        if (computerChoice === "paper") {
-          humanScore += 1;
-          console.log(`You won, You chose ${humanChoice} and computer chose ${computerChoice}`);
-        } else if (computerChoice === "rock") {
-          computerScore += 1;
-          console.log(`Computer won, You chose ${humanChoice} and computer chose ${computerChoice}`);
-        }
-        break;
-    }
-  };
-
   for (let i = 1; i <= 5; i++) {
     playARound(getHumanChoice(), getComputerChoice());
   }
-  if(humanScore > computerScore) {
+  if (humanScore > computerScore) {
     return `You won!`;
-  } else if(computerScore > humanScore) {
+  } else if (computerScore > humanScore) {
     return `Computer won!`;
   } else {
-    return `It's a tie.`
+    return `It's a tie.`;
   }
   return `Your score is ${humanScore} and computer's score is ${computerScore}`;
 };
 
-console.log(playGame());
+// console.log(playGame());
+let humanScore = 0;
+let computerScore = 0;
+const result = [];
+const playARound = (humanChoice, computerChoice) => {
+  if (humanChoice === computerChoice) {
+    console.log(
+      `It's a tie. You chose ${humanChoice} and the computer chose ${computerChoice}`
+    );
+  }
+  switch (humanChoice) {
+    case "rock":
+      if (computerChoice === "scissors") {
+        humanScore += 1;
+        console.log(
+          `You won, You chose ${humanChoice} and computer chose ${computerChoice}`
+        );
+      } else if (computerChoice === "paper") {
+        computerScore += 1;
+        console.log(
+          `Computer won, You chose ${humanChoice} and computer chose ${computerChoice}`
+        );
+      }
+      break;
+
+    case "paper":
+      if (computerChoice === "rock") {
+        humanScore += 1;
+        console.log(
+          `You won, You chose ${humanChoice} and computer chose ${computerChoice}`
+        );
+      } else if (computerChoice === "scissors") {
+        computerScore += 1;
+        console.log(
+          `Computer won, You chose ${humanChoice} and computer chose ${computerChoice}`
+        );
+      }
+      break;
+    case "scissors":
+      if (computerChoice === "paper") {
+        humanScore += 1;
+        console.log(
+          `You won, You chose ${humanChoice} and computer chose ${computerChoice}`
+        );
+      } else if (computerChoice === "rock") {
+        computerScore += 1;
+        console.log(
+          `Computer won, You chose ${humanChoice} and computer chose ${computerChoice}`
+        );
+      }
+      break;
+  }
+};
+
+playARound(getComputerChoice(), getHumanChoice());
